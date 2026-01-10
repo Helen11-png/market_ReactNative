e3daa8f78bcef3f1767d52f3b2f36f94f13f78c7
alena <alena.nik.07@mail.ru>
2026-01-11 01:44:28 +0300

    Added to GitHub

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome!</Text>
+      <Text>This app is just being made</Text>
+      <Text>just trust the process</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
